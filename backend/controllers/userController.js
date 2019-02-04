17de0c57196a521abab0b0e92117c1767863c4f7
aurelijaa <aurelija@czv.lt>
2019-02-04 22:01:43 +0200

    controlers and models

---
+++ backend/controllers/userController.js
@@ -0,0 +1,81 @@
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const config = require('../config/config')
+let {
+  User
+} = require("../models/userModel")
+
+
+let createUser = (req, res) => {
+  let data = req.body
+  let user = new User()
+  // user = data
+  user.name = data.name
+  user.email = data.email
+  user.nickname=data.nickname
+  user.password = data.password
+  user.save().then((user, err) => {
+    res.json(user)
+  }).catch((error) => {
+    res.json(error);
+  });
+
+}
+
+let login = (req, res) => {
+  let data = req.body
+  User.find({
+      $or:[
+        {'email':user.email}, 
+        {'nickname:': user.nickname}
+      ]
+  }).then((user) => {
+    if (!user) {
+      res.json("User doesnt exist or incorect user name. Maby you have to register before login.")
+      return
+    }
+    bcrypt.compare(data.password, user.password, (err, resp) => {
+      if (resp) {
+        let access = 'auth'
+        let token = jwt.sign({
+          _id: user._id.toHexString(),
+          access
+        }, config.superSecret).toString()
+        user.tokens.push({
+          access,
+          token
+        })
+        user.save().then(() => {
+          res.header('x-auth', token).json(user)
+        })
+      } else {
+        res.json("Incorrect password")
+      }
+    })
+
+  }).catch((error) => {
+    res.json("Something went wrong, please try again")
+  })
+}
+
+let logout = (req, res) => {
+  let token = req.token
+  req.user.update({
+    $pull: {
+      tokens: {
+        token
+      }
+    }
+  }).then(() => {
+    res.json("logged out")
+  })
+}
+
+
+
+
+module.exports = {
+  createUser,
+  login,
+  logout
+}
---
+++ backend/model/userModel.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose")
+const validator = require('validator')
+const bcrypt = require('bcrypt')
+
+let UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique:true,
+    minlength: 2
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 5,
+    validate: {
+      validator: validator.isEmail,
+      message: "{VALUE} is not valid email"
+    }
+  },
+  nickname: {
+    type: String,
+    required: true,
+    unique:true,
+    minlength: 5
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
+})
+
+UserSchema.pre("save", function(next) {
+  let user = this
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash
+        next()
+      })
+    })
+  } else {
+    next()
+  }
+})
+
+let User = mongoose.model("Users", UserSchema)
+module.exports = {
+  User
+}
